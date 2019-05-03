59edf51cfae2238b0063ac45b9fc307b306e4b82
gumnn <arita37@users.noreply.github.com>
2019-05-03 22:50:45 +0900

    Update main.sh

--- t_github_template/main.sh
+++ t_github_template/main.sh
@@ -2,20 +2,29 @@
 
 ## /home/ubuntu/zbatch.sh
 ### nohup  /home/ubuntu/zbatch.sh  2>&1 | tee -a /home/ubuntu/zlog/zbatch_log.log
+## /home/ubuntu/aagit/aapackage/aapackage/batch/task_template/main.sh
 
-### Need to source when using SSH   ############################################
+
+### Need to source when using SSH   ###########################################
 echo "-Start Script-"
+
 source /home/ubuntu/.bashrc
-
-
 export PATH="/home/ubuntu/anaconda3/bin:$PATH"
 
 source activate base
-cd /home/ubuntu/
-whoami
+# cd /home/ubuntu/
+# whoami
 pwd
 which python
 which conda
+
+
+
+########Change to Script Folder ##############################################
+CUR_DIR=$(dirname "${0}")
+# echo "${CUR_DIR}"
+cd $CUR_DIR
+pwd
 
 
 ##############################################################################
@@ -23,10 +32,15 @@ which conda
 source activate py36
 which python
 
+
+
 python ./main_run.py
 
 
+
+
 ##############################################################################
+
 
 
 
